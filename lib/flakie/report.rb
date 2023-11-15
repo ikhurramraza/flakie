12b6c2485ad803bf58d37db9c56dd13caae2c979
Khurram Raza <ikhurramraza@gmail.com>
2023-11-15 02:24:19 +0100

    fix: fix namespace

--- lib/flakie/report.rb
+++ lib/flakie/report.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 module Flakie
-  class Engine
-    Report =
-      Data.define(:runs) do
-        def last_run = runs.last
-        def empty? = runs.empty?
-      end
-  end
+  Report =
+    Data.define(:runs) do
+      def last_run = runs.last
+      def empty? = runs.empty?
+    end
 end
--- lib/flakie/run.rb
+++ lib/flakie/run.rb
@@ -1,11 +1,9 @@
 # frozen_string_literal: true
 
 module Flakie
-  class Engine
-    Run =
-      Data.define(:status, :duration, :number) do
-        def success? = status == true
-        def failure? = status == true
-      end
-  end
+  Run =
+    Data.define(:status, :duration, :number) do
+      def success? = status == true
+      def failure? = status == true
+    end
 end
