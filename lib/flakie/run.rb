4610b2a86e4105ce500bcedfeba5d01a6adafc72
Khurram Raza <ikhurramraza@gmail.com>
2024-03-22 05:02:20 +0100

    fix: fix `Flakie::Run#failure` method

--- lib/flakie/run.rb
+++ lib/flakie/run.rb
@@ -4,6 +4,6 @@ module Flakie
   Run =
     Data.define(:status, :duration, :number) do
       def success? = status == true
-      def failure? = status == true
+      def failure? = !success?
     end
 end
